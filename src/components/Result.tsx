df4452f5afe0e90d5c0c27ff69e645908bc25e20
Horacio <horacio.schumann@gmail.com>
2023-02-28 18:55:59 -0300

    feat: adicionado componente Result

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,4 +1,16 @@
+import { Link } from "react-router-dom";
+
+import "../styles/Result.css";
+
 export default function Result(props: any) {
   const { quizQuestionLength, numberOfCorrectAnswers } = props;
-  return <></>;
+  return (
+    <div className="results">
+      <p>Your Results</p>
+      <h1>
+        {numberOfCorrectAnswers}/{quizQuestionLength}
+      </h1>
+      <Link to="/">Go Back</Link>
+    </div>
+  );
 }

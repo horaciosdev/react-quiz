3170cbf9959b42aad1cd2bc9f6d742b27948ed2a
Horacio <horacio.schumann@gmail.com>
2023-02-28 18:51:55 -0300

    fix: navbar removido

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
 
 export default function Layout() {
   return (
     <>
-      <Navbar />
       <Outlet />
     </>
   );

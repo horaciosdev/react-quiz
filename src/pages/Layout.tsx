d98d8de0eac7e6def9ae00001952cd03bab6c3e0
Horacio <horacio.schumann@gmail.com>
2023-03-01 14:39:04 -0300

    feat: adição do header e do footer

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "./Footer";
+import "../styles/Layout.css";
 
 export default function Layout() {
   return (
     <>
-      <Outlet />
+      <Header />
+      <main className="main-content">
+        <Outlet />
+      </main>
+      <Footer />
     </>
   );
 }
